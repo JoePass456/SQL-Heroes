572fb1dd5a3cc0090191423580bfd69f7d4c1af8
Joe Passafiume <joepass456@gmail.com>
2020-11-06 10:04:04 -0500

    demo day version

--- index.php
+++ index.php
@@ -14,21 +14,29 @@ if ($conn->connect_error) {
 // echo "Connected successfully";
 
 $sql = "SELECT id, name, about_me, biography FROM heroes";
-$result = $conn->query($sql);
+$all_heroes = $conn->query($sql);
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $name = $_POST['name'];
+  $aboutme = $_POST['aboutme'];
+  $bio = $_POST['bio'];
 
-$conn->close();
+  $sql = "INSERT INTO heroes (name, about_me, biography)
+VALUES('$name', '$aboutme', '$bio')";
+  if ($conn->query($sql) === TRUE) {
+    echo 'New Record created successfully';
+  }
+}
 
-$state = "";
-
+$state = "";;
 
 
 ?>
 
 <!doctype html>
 <html lang="en">
-
-<head>
+  
+  <head>
   <title>Title</title>
   <!-- Required meta tags -->
   <meta charset="utf-8">
@@ -40,23 +48,111 @@ $state = "";
 
 <body>
   <ul>
-  <?php 
+    <?php
+    $state = $_GET["state"];
+    $profile_id = $_GET["profileid"];
 
-if ($result->num_rows > 0) {
-  // output data of each row
-  while ($row = $result->fetch_assoc()) {
-    // echo "<pre>" . print_r($row, 1) . "</pre>";
-    echo "<li><a href='index.php?profileid=" . $row["id"] . "&state=profile'> id: " . $row["id"] . " - Name: " . $row["name"] . " " . $row["about_me"] . "</a></li>";
-  }
-} else {
-  echo "0 results";
-}
+    if ($state == "delete") {
+      $sql = "DELETE FROM heroes WHERE id=$profile_id";
+      $all_heroes = $conn->query($sql);
+
+      if ($conn->query($sql) === TRUE) {
+        echo "Record deleted successfully";
+      } else {
+        echo "Error deleting record: " . $conn->error;
+      }
+      echo "<button><a href='index.php'>Back</a></button>";
+    }
+
+    if ($all_heroes->num_rows > 0) {
+      // output data of each row
+      while ($row = $all_heroes->fetch_assoc()) {
+        // echo "<pre>" . print_r($row, 1) . "</pre>";
+        echo "<li>
+        <a href='index.php?profileid="
+          . $row["id"]
+          . "&state=profile'>"
+          . $row["name"]
+          . "</a>
+          <a href='index.php?profileid="
+          . $row["id"]
+          . "&state=delete'>-- DELETE --</a>        
+          </li>";
+      }
+    } else {
+
+      echo "0 results";
+    }
+
+    if ($profile_id != 0) {
+      // echo "got id";
+      $sql = "SELECT id, name, about_me, biography FROM heroes WHERE id = '$profile_id'";
+      $profile = $conn->query($sql);
+      if ($profile->num_rows > 0) {
+        // output data of each row
+        while ($row = $profile->fetch_assoc()) {
+          // echo "<pre>" . print_r($row, 1) . "</pre>";
+          echo "<h3>" . $row["name"] . "</h3>";
+          echo "<p>" . $row["about_me"] . "</p>";
+          echo "<p>" . $row["biography"] . "</p>";
+        }
+      } else {
+        echo "0 results";
+      }
+      $sql = "SELECT ability FROM abilities 
+      INNER JOIN ability_hero WHERE id = '$profile_id'";
+      $profile = $conn->query($sql);
+      if ($profile->num_rows > 0) {
+        // output data of each row
+        while ($row = $profile->fetch_assoc()) {
+          // echo "<pre>" . print_r($row, 1) . "</pre>";
+          echo "<h3>Ability: " . $row["name"] . "</h3>";
+          
+        }
+      } else {
+        echo "0 results";
+      }
+
+    } else {
+      //echo "no id";
+    }
+
+    ?>
+  </ul>
+  <div>
+    <?php
+    // echo $state . $profile_id;
+    // $row = $result->fetch_assoc();
+    // var_dump($result);
+
+    ?>
+  </div>
+  <form action='<?php echo $_SERVER['PHP_SELF']; ?>' method='post'>
+    <div class="form-group">
+      <label for="name">New Hero Name:</label>
+      <input name="name" type="text" class="form-control" id="name">
+    </div>
+    <div class="form-group">
+      <label for="aboutme">About Me:</label>
+      <input name="aboutme" type="text" class="form-control" id="aboutme">
+    </div>
+    <div class="form-group">
+      <label for="bio">Biography:</label>
+      <input name="bio" type="text" class="form-control" id="bio">
+    </div>
+    <button type="submit" class="btn btn-default">Submit</button>
+  </form>
+  <?php
 
 
-  echo "state = " . $_GET["state"] . "</br>";
+  
 
-?></ul>
-  <button>See your friends</button>
+  $conn->close();
+  ?>
+
+
+
+  <!-- <button></button> -->
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
